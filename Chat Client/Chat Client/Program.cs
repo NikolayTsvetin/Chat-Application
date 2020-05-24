f23e64baebfe64f5d85a68e076634658c555181d
nikolaytsvetin <nikolaytsvetin@gmail.com>
2020-05-24 20:29:38 +0300

    Stable - working server with synchronious client accepting.

---
+++ Chat Client/Chat Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = String.Empty;
+
+            while (!message.Equals("Exit"))
+            {
+                TcpClient client = new TcpClient("127.0.0.1", 8000);
+                NetworkStream stream = client.GetStream();
+
+                Console.WriteLine("Please, type a message: ");
+
+                message = Console.ReadLine();
+                Byte[] data = Encoding.ASCII.GetBytes(message);
+
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine($"Message sent to server: {message}");
+                stream.Flush();
+                data = new Byte[256];
+                int serverResponse = stream.Read(data, 0, data.Length);
+                string serverResponseMessage = Encoding.ASCII.GetString(data);
+
+                Console.WriteLine($"Received response from server: {serverResponseMessage}");
+            }
+
+            //stream.Dispose();
+            //stream.Close();
+            //client.Close();
+        }
+    }
+}
---
+++ Chat Server/Chat Server/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // setup - change if needed
+            IPAddress address = IPAddress.Parse("127.0.0.1");
+            const int port = 8000;
+
+            TcpListener server = new TcpListener(address, port);
+            server.Start();
+
+            Console.WriteLine($"Server started on address: {address.ToString()} and port: {port}.");
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Waiting for client connection...");
+
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Client connected!");
+
+                    NetworkStream stream = client.GetStream();
+                    Byte[] data = new Byte[256];
+                    string message = String.Empty;
+
+                    stream.Read(data, 0, data.Length);
+                    message = Encoding.ASCII.GetString(data);
+
+                    Console.WriteLine($"Received: {message}");
+
+
+                    byte[] returnToClient = Encoding.ASCII.GetBytes(message);
+
+                    // Send back a response.
+                    stream.Write(returnToClient, 0, returnToClient.Length);
+                    Console.WriteLine("Sent: {0}", message);
+                    //stream.Dispose();
+                    //client.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ëxception thrown: {ex.Message}");
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Shutting server down...");
+                server.Stop();
+            }
+        }
+    }
+}
